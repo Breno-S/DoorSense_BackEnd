1d9b9057ed153deadc963257fff18eb6f8d958ae
Rodrigo-Martins-Mateus <115177872+Rodrigo-Martins-Mateus@users.noreply.github.com>
2023-08-31 16:50:18 -0300

    Verificação do Token(API)

--- api/salas/create/index.php
+++ api/salas/create/index.php
@@ -1,6 +1,9 @@
 <?php
 include_once '../../include/conexao.php';
 include_once '../../include/funcoes.php';
+require 'vendor/autoload.php'; // Certifique-se de incluir o autoload do Firebase JWT
+
+use \Firebase\JWT\JWT;
 
 // Headers
 header("Access-Control-Allow-Origin: *");
@@ -15,39 +18,63 @@ $response = [];
 $method = $_SERVER['REQUEST_METHOD'];
 
 if ($method == 'POST') {
-    $json_data = file_get_contents('php://input');
-    $data = json_decode($json_data, true);
+    // Verifica a presença do token de autorização
+    $headers = getallheaders();
+    $authorizationHeader = isset($headers['Authorization']) ? $headers['Authorization'] : '';
 
-    if ($data !== null && is_array($data)) { //Se são válidos e se são um array.
-        if (isset($data['nome']) && isset($data['numero']) && is_string($data['nome']) && is_string($data['numero'])) {
-            $nome_sala = trim($data['nome']); //Remove espaço no início e final de uma string
-            $numero_sala = trim($data['numero']);
+    // Verifica se o cabeçalho de autorização está no formato "Bearer <token>"
+    list(, $token) = explode(' ', $authorizationHeader);
 
-            
+    if (!$token) {
+        http_response_code(401);
+        echo json_encode(['status' => '401 Unauthorized', 'message' => 'Token de autorização ausente']);
+        exit;
+    }
 
-            if ($nova_sala = create_sala($conn, $nome_sala, $numero_sala)) {
-                $response['status'] = "200 OK";
-                $response['message'] = "Sala adicionada com sucesso";
-                $response['data'] = [
-                    "id" => $nova_sala['ID_SALA'],
-                    "nome" => $nova_sala['NOME_SALA'],
-                    "numero" => $nova_sala['NUMERO_SALA'],
-                    "arduino" => null,
-                    "status" => null
-                ];
+    // Chave secreta usada para assinar e verificar o token
+    $key = 'arduino';
+
+    try {
+        // Decodifica o token usando a chave secreta
+        $decoded = JWT::decode($token, $key);
+
+        $json_data = file_get_contents('php://input');
+        $data = json_decode($json_data, true);
+
+        if ($data !== null && is_array($data)) { //Se são válidos e se são um array.
+            if (isset($data['nome']) && isset($data['numero']) && is_string($data['nome']) && is_string($data['numero'])) {
+                $nome_sala = trim($data['nome']); //Remove espaço no início e final de uma string
+                $numero_sala = trim($data['numero']);
+
+                if ($nova_sala = create_sala($conn, $nome_sala, $numero_sala)) {
+                    $response['status'] = "200 OK";
+                    $response['message'] = "Sala adicionada com sucesso";
+                    $response['data'] = [
+                        "id" => $nova_sala['ID_SALA'],
+                        "nome" => $nova_sala['NOME_SALA'],
+                        "numero" => $nova_sala['NUMERO_SALA'],
+                        "arduino" => null,
+                        "status" => null
+                    ];
+                } else {
+                    $response['status'] = "500 Internal Server Error";
+                    $response['message'] = "Erro ao criar a sala";
+                }
             } else {
-                $response['status'] = "500 Internal Server Error";
-                $response['message'] = "Erro ao criar a sala";
+                $response['status'] = "400 Bad Request"; // requisição do cliente não está correta
+                $response['message'] = "Parâmetros inválidos";
             }
         } else {
-            $response['status'] = "400 Bad Request"; // requisição do cliente não está correta
-            $response['message'] = "Parâmetros inválidos";
+            $response['status'] = "400 Bad Request";  // requisição do cliente não está correta
+            $response['message'] = "JSON inválido";
         }
-    } else {
-        $response['status'] = "400 Bad Request";  // requisição do cliente não está correta
-        $response['message'] = "JSON inválido";
+    } catch (Exception $e) {
+        http_response_code(401);
+        echo json_encode(['status' => '401 Unauthorized', 'message' => 'Acesso não autorizado: ' . $e->getMessage()]);
+        exit;
     }
 } else {
+    http_response_code(405);
     $response['status'] = "405 Method Not Allowed";  
     $response['message'] = "Método da requisição inválido";
 }
--- api/salas/delete/index.php
+++ api/salas/delete/index.php
@@ -1,6 +1,9 @@
 <?php
 include_once '../../include/conexao.php';
 include_once '../../include/funcoes.php';
+require 'vendor/autoload.php'; // Certifique-se de incluir o autoload do Firebase JWT
+
+use \Firebase\JWT\JWT;
 
 // Headers
 header("Access-Control-Allow-Origin: *");
@@ -15,31 +18,57 @@ $response = [];
 $method = $_SERVER['REQUEST_METHOD'];
 
 if ($method == 'DELETE') {
-    $json_data = file_get_contents('php://input');
-    $data = json_decode($json_data, true);
+    // Verifica a presença do token de autorização
+    $headers = getallheaders();
+    $authorizationHeader = isset($headers['Authorization']) ? $headers['Authorization'] : '';
 
-    if ($data !== null && is_array($data)) { //Se são válidos e se são um array.
-        if (isset($data['id']) && is_numeric($data['id'])) { //se o 'id' está presente nos dados e se o valor associado a essa chave é um número válido.
-            $id_sala = intval($data['id']); 
+    // Verifica se está no formato "Bearer <token>"
+    list(, $token) = explode(' ', $authorizationHeader);
 
-            if (delete_sala($conn, $id_sala)) {
-                $response['status'] = "200 OK";
-                $response['message'] = "Sala deletada com sucesso";
+    if (!$token) {
+        http_response_code(401);
+        echo json_encode(['status' => '401 Unauthorized', 'message' => 'Token de autorização ausente']);
+        exit;
+    }
+
+    // Chave secreta 
+    $key = 'arduino';
+
+    try {
+        // Decodifica o token 
+        $decoded = JWT::decode($token, $key);
+
+
+        $json_data = file_get_contents('php://input');
+        $data = json_decode($json_data, true);
+
+        if ($data !== null && is_array($data)) { //Se são válidos e se são um array.
+            if (isset($data['id']) && is_numeric($data['id'])) { //se o 'id' está presente nos dados e se o valor associado a essa chave é um número válido.
+                $id_sala = intval($data['id']); 
+
+                if (delete_sala($conn, $id_sala)) {
+                    $response['status'] = "200 OK";
+                    $response['message'] = "Sala deletada com sucesso";
+                } else {
+                    $response['status'] = "500 Internal Server Error"; // caso de erro interno.
+                    $response['message'] = "Erro ao deletar sala: " . mysqli_error($conn);
+                }
             } else {
-                $response['status'] = "500 Internal Server Error"; // caso de erro interno.
-                $response['message'] = "Erro ao deletar sala: " . mysqli_error($conn);
+                $response['status'] = "400 Bad Request"; // caso a validação de entrada falhe.
+                $response['message'] = "ID da sala inválido";
             }
         } else {
-            $response['status'] = "400 Bad Request"; // caso a validação de entrada falhe.
-            $response['message'] = "ID da sala inválido";
+            http_response_code(400);
+            $response['status'] = "400 Bad Request";
+            $response['message'] = "JSON inválido";
         }
-    } else {
-        http_response_code(400);
-        $response['status'] = "400 Bad Request";
-        $response['message'] = "JSON inválido";
+    } catch (Exception $e) {
+        http_response_code(401);
+        echo json_encode(['status' => '401 Unauthorized', 'message' => 'Acesso não autorizado: ' . $e->getMessage()]);
+        exit;
     }
 } else {
-    http_response_code(400);
+    http_response_code(405);
     $response['status'] = "405 Method Not Allowed"; //solicitações de método não permitidas.
     $response['message'] = "Método da requisição inválido";
 }
