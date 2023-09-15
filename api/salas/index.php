3ed359e9626a9ffe8f35061e9b7e5db044832bba
Breno-S <114250412+Breno-S@users.noreply.github.com>
2023-09-15 16:28:54 -0300

    att create
    mensagens de erro mais descritivos. Try-catch somente no JWT::decode

--- api/salas/index.php
+++ api/salas/index.php
@@ -12,6 +12,9 @@ header("Access-Control-Allow-Methods: GET");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Content-Type: application/json; charset=UTF-8");
 
+// Parâmetros permitidos pelo endpoint
+$allowed_params = ["id"];
+
 // Response (deve ser um array associativo)
 $response = [];
 
@@ -35,10 +38,6 @@ if ($method == 'GET') {
     if (preg_match('/^Bearer [A-Za-z0-9\-._~+\/]+=*$/', $authorizationHeader)) {
         list(, $token) = explode(' ', $authorizationHeader);
     } else {
-        $token = false;
-    }
-
-    if (!$token) {
         http_response_code(401);
         echo json_encode(['status' => '401 Unauthorized', 'message' => 'Token de autorização ausente']);
         exit;
@@ -50,54 +49,91 @@ if ($method == 'GET') {
     try {
         // Decodifica o token usando a chave secreta
         $decoded = JWT::decode($token, new Key($key, 'HS256'));
-
-        $json_data = file_get_contents('php://input');
-        $data = json_decode($json_data, true);
-
-        if (!empty($data)) {
-            if (isset($data['id'])) {
-                $id = intval($data['id']);
-                if ($sala = get_sala($conn, $id)) {
-                    $response['status'] = "200 OK";
-                    $response['message'] = "Sala encontrada";
-                    $response['data'] = [
-                        "id" => $sala['ID_SALA'],
-                        "nome" => $sala['NOME_SALA'],
-                        "numero" => $sala['NUMERO_SALA'],
-                        "arduino" => $sala['ARDUINO_SALA'],
-                        "status" => $sala['STATUS_SALA']
-                    ];
-                } else {
-                    http_response_code(404);
-                    $response['status'] = "404 Not Found";
-                    $response['message'] = "Sala não encontrada";
-                }
-            } else {
-                $response['status'] = "400 Bad Request";
-                $response['message'] = "Parâmetros inválidos";
-            }
-        } else {
-            if ($all_salas = get_all_salas($conn)) {
-                $response['status'] = "200 OK";
-                $response['message'] = "Todas as salas registradas";
-
-                $total = get_total_salas($conn);
-
-                $response['data'] = [
-                    "total" => $total,
-                    "salas" => []
-                ];
-
-                foreach ($all_salas as $indice => $dados_sala) {
-                    array_push($response['data']['salas'], $dados_sala);
-                }
-            }
-        }
     } catch (Exception $e) {
         http_response_code(401);
         echo json_encode(['status' => '401 Unauthorized', 'message' => 'Acesso não autorizado: ' . $e->getMessage()]);
         exit;
     } 
+
+    // Verifica se há um body na requisição
+    if ($json_data = file_get_contents('php://input')) {
+        // Verifica se o JSON é válido
+        if (!($data = json_decode($json_data, true))) {
+            http_response_code(400); 
+            $response['status'] = "400 Bad Request";
+            $response['message'] = "Body mal estruturado";
+            echo json_encode($response);
+            exit;
+        }
+
+        // Obtém todas as chaves do JSON do body
+        $body_params = array_keys($data);
+
+        // Verifica se há chaves inválidas na requisição
+        if (array_diff($body_params, $allowed_params)) {
+            http_response_code(400);
+            $response['status'] = "400 Bad Request";
+            $response['message'] = "Parâmetros deconhecidos na requisição";
+            echo json_encode($response);
+            exit;
+        }
+        
+        // Obter sala específica
+        if (isset($data['id'])) {
+            // Verifica se o valor da chave id é numérico
+            if (filter_var($data['id'], FILTER_VALIDATE_INT) === false ) {
+                http_response_code(400);
+                $response['status'] = "400 Bad Request";
+                $response['message'] = "Argumento do id não é numérico";
+                echo json_encode($response);
+                exit;
+            }
+
+            $id = $data['id'];
+
+            if ($sala = get_sala($conn, $id)) {
+                $response['status'] = "200 OK";
+                $response['message'] =   "Sala encontrada";
+                $response['data'] = [
+                    "id" => $sala['ID_SALA'],
+                    "nome" => $sala['NOME_SALA'],
+                    "numero" => $sala['NUMERO_SALA'],
+                    "arduino" => $sala['ARDUINO_SALA'],
+                    "status" => $sala['STATUS_SALA']
+                ];
+            } else {
+                http_response_code(404);
+                $response['status'] = "404 Not Found";
+                $response['message'] = "Sala não encontrada";
+            }
+        } else {
+            // roda quando o valor da chave id é null
+            http_response_code(400);
+            $response['status'] = "400 Bad Request";
+            $response['message'] = "Argumento inválido";
+        }
+    } else {
+        // obter todas as salas
+        if ($all_salas = get_all_salas($conn)) {
+            $response['status'] = "200 OK";
+            $response['message'] = "Todas as salas registradas";
+
+            $total = get_total_salas($conn);
+
+            $response['data'] = [
+                "total" => $total,
+                "salas" => []
+            ];
+
+            foreach ($all_salas as $indice => $dados_sala) {
+                array_push($response['data']['salas'], $dados_sala);
+            }
+        } else {
+            http_response_code(500);
+            $response['status'] = "500 Internal Server";
+            $response['message'] = "Erro ao obter todas as salas";
+        }
+    }
 } else {
     http_response_code(400);
     $response['status'] = "400 Bad Request";
