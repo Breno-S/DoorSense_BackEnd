cb0f26f3c51a94366083260f548f9b3531fd9f05
Breno-S <114250412+Breno-S@users.noreply.github.com>
2023-10-26 22:12:45 -0300

    mysql environment variables
    obter as variáveis de conexão (separadas do repositório)

--- include/conexao.php
+++ include/conexao.php
@@ -1,8 +1,18 @@
 <?php
-    $servidor = "localhost";    // $servidor 
-    $usuario = "root";          
-    $senha = "";
-    $dbname = "doorsense";
-    
-    $conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
+$host = getenv('DB_HOST');
+$username = getenv('DB_USER');
+$password = getenv('DB_PASSWORD');
+$database = getenv('DB_DATABASE');
+$port = getenv('DB_PORT');
+
+// Inicializar o objeto mysqli
+$conn = mysqli_init();
+
+// Configurações de SSL/TLS
+mysqli_ssl_set($conn, NULL, NULL, "C:\\Users\\breno\\Downloads\\DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+
+// Estabelecer a conexão
+if (!mysqli_real_connect($conn, $host, $username, $password, $database, $port, NULL, MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT)) {
+    die("Conexão falhou: " . mysqli_connect_error());
+}
 ?>
