31b613322e222f893c0324407459a8e16ad14f2d
Breno-S <114250412+Breno-S@users.noreply.github.com>
2023-11-12 17:57:16 -0300

    test mysql

--- index.php
+++ index.php
@@ -1,7 +1,4 @@
 <?php
-
-phpinfo();
-
 $hostname = getenv('DB_HOST');
 $username = getenv('DB_USERNAME');
 $password = getenv('DB_PASSWORD');
@@ -9,10 +6,27 @@ $database = getenv('DB_DATABASE');
 $port     = getenv('DB_PORT');
 $ca_path  = getenv('DB_CA_CERT_PATH');
 
-echo $hostname;
-echo $username;
-echo $password;
-echo $database;
-echo $port;
-echo $ca_path;
+if (!empty($hostname) &&
+    !empty($username) &&
+    !empty($password) &&
+    !empty($database) &&
+    !empty($port) &&
+    !empty($ca_path) ) 
+{
+    // Inicializar o objeto mysqli
+    $conn = mysqli_init();
+
+    // Configurações de SSL/TLS
+    mysqli_ssl_set($conn, NULL, NULL, $ca_path, NULL, NULL);
+
+    // Estabelecer a conexão
+    if (!mysqli_real_connect($conn, $hostname, $username, $password, $database, $port)) {
+        die("Conexão falhou: " . mysqli_connect_error());
+    } else {
+		echo "Deu certo!";
+	}
+} else {
+    $conn = mysqli_connect("localhost", "root", "", "doorsense");
+}
+
 ?>
