3bf70ee7e554f95069935a785661d373446a7ed2
Breno-S <114250412+Breno-S@users.noreply.github.com>
2023-11-12 17:43:40 -0300

    env vars azure

--- index.php
+++ index.php
@@ -2,4 +2,17 @@
 
 phpinfo();
 
+$hostname = getenv('DB_HOST');
+$username = getenv('DB_USERNAME');
+$password = getenv('DB_PASSWORD');
+$database = getenv('DB_DATABASE');
+$port     = getenv('DB_PORT');
+$ca_path  = getenv('DB_CA_CERT_PATH');
+
+echo $hostname;
+echo $username;
+echo $password;
+echo $database;
+echo $port;
+echo $ca_path;
 ?>
